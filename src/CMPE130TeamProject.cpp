8791740238c7376526345010cc310fc252615355
DavidWei2022 <102999460+DavidWei2022@users.noreply.github.com>
2022-04-04 14:13:50 -0700

    Update CMPE130TeamProject.cpp

--- src/CMPE130TeamProject.cpp
+++ src/CMPE130TeamProject.cpp
@@ -12,5 +12,6 @@ using namespace std;
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	cout << "More money more problems"; 
+	cout << "David made some changes" <<endl;
 	return 0;
 }
