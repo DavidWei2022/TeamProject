b761476c7069362e419c2403afefd913b3a6c00d
DavidWei2022 <102999460+DavidWei2022@users.noreply.github.com>
2022-04-07 18:12:55 -0700

    Add files via upload

--- src/CMPE130TeamProject.cpp
+++ src/CMPE130TeamProject.cpp
@@ -11,15 +11,6 @@ using namespace std;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-
-	cout << "More money more problems"; 
 	cout << "David made some changes" <<endl;
-  cout << "More money more problems";
-	cout << "Lets do a pull request";
-
-	
-	cout<< "hi"<<endl;
-	
-	
 	return 0;
 }
